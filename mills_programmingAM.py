ce49a856446beec5563648415a4e7ef2f7749642
rigbone10 <34494438+rigbone10@users.noreply.github.com>
2019-04-19 10:31:22 -0500

    Add files via upload

--- mills_programmingAM.py
+++ mills_programmingAM.py
@@ -1,17 +1,22 @@
 import random
 monsters = ["Goblin", "Ghost", "Dragon"]
 weapons = ["Sword", "sword", "Bow", "bow", "Magic", "magic"]
-targets = ["Head", "head", "Body", "body", "Random", "random"]
+targets = ["Head", "Body", "Random"]
+random_damage = [0, 1, 2, 3, 4, 5]
 gob_health = 10
 gst_health = 15
 drag_health = 20
 health = 0
+head = 0
+body = 0
 #Randomly picks monster 
 monster_pick = random.SystemRandom()
-
 monster_name = monster_pick.choice(monsters )
 target_pick= random.SystemRandom()
 player_target = target_pick.choice(targets )
+"""random_pick = random.SystemRandom()
+random_dam = random_pick.choice(random_damage)"""
+
 def monster_pick(monster_name ):
     
     if  monster_name == "Goblin":                
@@ -22,11 +27,15 @@ def monster_pick(monster_name ):
         
     elif  monster_name == "Dragon":                
         health = drag_health
-         
-"""def weapon_damage(monster_name, weapon_choice, targets):
-    if weapon_choice == weapons[:2]:
+    
+def weapon_damage(monster_name, weapon_choice, targets):
+    if weapon_choice == weapons[:2] and monster_name == "Goblin":
+        if target_pick == "Random":
+        
+        
+        
+        
 
-def body_pick(palyer_target ):"""
     
         
         
@@ -44,7 +53,8 @@ def main():
                 print "Use Sword, Bow, or Magic?"
                 weapon_choice = raw_input()
                 if weapon_choice in ["Sword", "sword"] :
-                    print "You picked a sword"                                        
+                    print "You picked a sword"
+                    
                 elif weapon_choice in ["Bow", "bow"] :
                     print "You picked a bow"
                 elif weapon_choice in ["Magic", "magic"] : 
